e47332dfdc3f0ed788262c26b248252a89dc35ef
helinyu <helinyu@users.noreply.github.com>
2024-10-02 04:17:49 +0800

    1.0.0

--- VCB.rb
+++ VCB.rb
@@ -1,27 +1,19 @@
-class VCB < Formula
+
+  cask "VCB" do
+    version "1.0.0"  # 软件版本
+    sha256 "03f34b77688517920d4803dfb74f4d41fc731a66c4174cb1aa41d1e6a97d68fa"  # .dmg 文件的 SHA256 校验和
+    url "https://github.com/VCBSstudio/VCB/releases/download/#{version}/VCB.#{version}.dmg"  # 你托管 vcb.dmg 的网址
     name "VCB"
     desc "like obs"
     homepage "https://github.com/VCBSstudio/VCB"
-    url "https://github.com/VCBSstudio/VCB/releases/download/#{version}/VCB.#{version}.dmg" # 指向你的源代码包
-    sha256 "03f34b77688517920d4803dfb74f4d41fc731a66c4174cb1aa41d1e6a97d68fa" # 你可以使用 `shasum -a 256 yourapp-1.0.0.tar.gz` 获取
-
-    livecheck do
-        url "https://github.com/VCBSstudio/VCB/releases/latest"
-        strategy :page_match
-        regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
-      end
-      
-    #   depends_on macos: ">= :monterey"
-    
-      app "VCB.app"
-
-      def install
-        bin.install "VCB"  # 确保这部分正确
-      end
-    
-    #   zap trash: [
-    #     "~/Library/HTTPStorages/com.lihaoyun6.QuickRecorder",
-    #     "~/Library/Preferences/com.lihaoyun6.QuickRecorder.plist",
-    #   ]
+  
+    # 安装应用程序
+    app "VCB.app"
+  
+    # 可选的卸载脚本或后处理
+    zap trash: [
+      "~/Library/Preferences/com.your_company.vcb.plist",
+      "~/Library/Application Support/VCB",
+    ]
   end
   
