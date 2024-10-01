948561e8f8972fc2eb3392c768a74245a9796091
helinyu <helinyu@users.noreply.github.com>
2024-10-02 04:25:27 +0800

    1.0.0

--- Casks/VCB.rb
+++ Casks/VCB.rb
@@ -1,5 +1,5 @@
 
-  cask "VCB" do
+  cask "vcb" do
     version "1.0.0"  # 软件版本
     sha256 "03f34b77688517920d4803dfb74f4d41fc731a66c4174cb1aa41d1e6a97d68fa"  # .dmg 文件的 SHA256 校验和
     url "https://github.com/VCBSstudio/VCB/releases/download/#{version}/VCB.#{version}.dmg"  # 你托管 vcb.dmg 的网址
