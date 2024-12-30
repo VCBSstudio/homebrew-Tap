beb352b948bdb3ccae052cac7d5c9f7532ab5993
helinyu <helinyu@users.noreply.github.com>
2024-12-30 20:11:49 +0800

    Update ScreenCut to version v1.1.4

--- Casks/ScreenCut.rb
+++ Casks/ScreenCut.rb
@@ -1,8 +1,8 @@
 
   cask "screencut" do
-  version "v1.1.3"  # 软件版本
-  sha256 "b557dc7ed9970ff5f8679b275dbd0a5f321c89464f7fdcc9bedb24687f3c8796"  # .dmg 文件的 SHA256 校验和
-  url "https://github.com/VCBSstudio/ScreenCut/releases/download/v1.1.3/ScreenCut.dmg"  # 你托管 screencut.dmg 的网址
+  version "v1.1.4"  # 软件版本
+  sha256 "2aac3abb0d29f3e17f07fdc7b2c108f08a6d34345cc32206d8e991c6f61ce6ca"  # .dmg 文件的 SHA256 校验和
+  url "https://github.com/VCBSstudio/ScreenCut/releases/download/v1.1.4/ScreenCut.dmg"  # 你托管 screencut.dmg 的网址
     name "ScreenCut"
     desc "一个mac上的轻量级截图工具"
     homepage "https://github.com/VCBSstudio/ScreenCut"
